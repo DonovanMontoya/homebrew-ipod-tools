8c3d1d7d492677167723035e7b363f993a546dbd
Donovan Montoya <donovan.montoya1@gmail.com>
2026-01-22 15:29:03 -0500

    Fix URL for cdrip repository in Formula

--- Formula/cdrip.rb
+++ Formula/cdrip.rb
@@ -1,7 +1,7 @@
 class Cdrip < Formula
   desc "CLI to read/convert AIFF files to FLAC"
   homepage "https://github.com/DonovanMontoya/CD-Rip"
-  url "https://github.com/DonovanMontoya/CD-Rip/archive/refs/tags/v0.1.1.tar.gz"
+  url "https://github.com/DonovanMontoya/cdrip/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "052186b1a0289005d3f019a64c15d5569fac7fd7794a5d692543f706065081c0"
   license "MIT"
 
