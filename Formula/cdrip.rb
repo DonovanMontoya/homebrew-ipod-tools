50b103bbc8f9ceafd07957ad3d7e965907266dc3
Donovan Montoya <donovan.montoya1@gmail.com>
2026-01-22 11:42:58 -0500

    Updated SHA

--- Formula/cdrip.rb
+++ Formula/cdrip.rb
@@ -2,7 +2,7 @@ class Cdrip < Formula
   desc "CLI to read/convert AIFF files to FLAC"
   homepage "https://github.com/DonovanMontoya/CD-Rip"
   url "https://github.com/DonovanMontoya/CD-Rip/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_TARBALL_SHA256"
+  sha256 "940b3e0f3f961f173729955fb7ba9465e1b63206c40692fec10161edd5b05f20"
   license "MIT"
 
   depends_on "rust" => :build
