4f49d0049eb26a944c733450fbe803f37981baf0
Donovan Montoya <donovan.montoya1@gmail.com>
2026-05-29 17:23:42 -0400

    cdrip: bump url to v0.1.6 to match sha256

--- Formula/cdrip.rb
+++ Formula/cdrip.rb
@@ -1,7 +1,7 @@
 class Cdrip < Formula
   desc "CLI to read/convert AIFF files to FLAC"
   homepage "https://github.com/DonovanMontoya/CD-Rip"
-  url "https://github.com/DonovanMontoya/cdrip/archive/refs/tags/v0.1.1.tar.gz"
+  url "https://github.com/DonovanMontoya/cdrip/archive/refs/tags/v0.1.6.tar.gz"
   sha256 "fc39bc9df7bcc8978feaf31c4172372c8e5720b01b0decc0e20ea536b5449ff8"
   license "MIT"
 
